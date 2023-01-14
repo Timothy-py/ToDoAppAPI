44321d074f1eab45db1c1d9948e6592dcb7e5935
Timothy-py <adeyeyetimothy33@gmail.com>
2023-01-14 22:44:52 +0100

    setup auth signup route

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { AuthDto } from './dto';
+
+@Injectable()
+export class AuthService {
+
+  signup(authDto: AuthDto){
+    return 'User signup successfully'
+  }
+
+  // create(createAuthDto: CreateAuthDto) {
+  //   return 'This action adds a new auth';
+  // }
+
+  // findAll() {
+  //   return `This action returns all auth`;
+  // }
+
+  // findOne(id: number) {
+  //   return `This action returns a #${id} auth`;
+  // }
+
+  // update(id: number, updateAuthDto: UpdateAuthDto) {
+  //   return `This action updates a #${id} auth`;
+  // }
+
+  // remove(id: number) {
+  //   return `This action removes a #${id} auth`;
+  // }
+}
