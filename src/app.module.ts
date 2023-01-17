414f06fdc66e2213e47b8d28228ff7942c8abb2e
Timothy-py <adeyeyetimothy33@gmail.com>
2023-01-17 17:21:40 +0100

    setup user resource and basic get use api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
 import {ConfigModule} from '@nestjs/config'
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule, ConfigModule.forRoot({isGlobal: true})],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, PrismaModule, ConfigModule.forRoot({isGlobal: true}), UserModule]
 })
 export class AppModule {}
