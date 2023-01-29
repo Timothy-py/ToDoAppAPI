ff67f3b995cb786c9c78aba7d747b394bf90887f
Timothy-py <adeyeyetimothy33@gmail.com>
2023-01-29 22:52:25 +0100

    generated barebone todo resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import {ConfigModule} from '@nestjs/config'
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule, ConfigModule.forRoot({isGlobal: true}), UserModule]
+  imports: [AuthModule, PrismaModule, ConfigModule.forRoot({isGlobal: true}), UserModule, TodoModule]
 })
 export class AppModule {}
