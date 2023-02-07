26c4eff7bfa2aa307c3afb9884b8b282ac458b81
Timothy-py <adeyeyetimothy33@gmail.com>
2023-02-07 23:46:49 +0100

    implemented api for creating a comment

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Param, ParseIntPipe, Post, Request } from '@nestjs/common';
+import { CommentService } from './comment.service';
+import { CreateCommentDto } from './dto';
 
 @Controller('comment')
-export class CommentController {}
+export class CommentController {
+    constructor(private readonly commentService: CommentService){}
+
+    @Post('todo/:id')
+    createComment(@Body() dto: CreateCommentDto, @Param('id', ParseIntPipe) id:number){
+        return this.commentService.createComment(id, dto.text)
+    }
+}
--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class CommentService {}
+export class CommentService {
+    constructor(private readonly prisma: PrismaService){}
+
+    async createComment(todoId:number, text:string){
+        try {
+            const comment = await this.prisma.comment.create({
+                data: {
+                    todoId,
+                    text
+                }
+            })
+
+            return {
+                message: 'Comment created successfully',
+                data: comment
+            }
+        } catch (error) {
+            console.log(error)
+            return {
+                error: error.message
+            }
+        }
+    }
+}
