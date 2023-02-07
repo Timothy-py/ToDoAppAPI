e51f12756325a0b733f747d2eb61656abd3a3bc0
Timothy-py <adeyeyetimothy33@gmail.com>
2023-02-07 22:14:59 +0100

    generates Commment resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,9 @@ import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { TodoModule } from './todo/todo.module';
+import { CommentModule } from './comment/comment.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule, ConfigModule.forRoot({isGlobal: true}), UserModule, TodoModule]
+  imports: [AuthModule, PrismaModule, ConfigModule.forRoot({isGlobal: true}), UserModule, TodoModule, CommentModule]
 })
 export class AppModule {}
