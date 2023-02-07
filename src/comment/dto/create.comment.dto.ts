4fb67a4f7c4f1e86daed2dfd66497e7574631967
Timothy-py <adeyeyetimothy33@gmail.com>
2023-02-07 23:05:55 +0100

    updated dto for best practice

--- src/comment/dto/create.comment.dto.ts
+++ src/comment/dto/create.comment.dto.ts
@@ -2,10 +2,6 @@ import { IsNotEmpty, IsNumber, IsString } from "class-validator";
 
 export class CreateCommentDto {
     @IsNotEmpty()
-    @IsNumber()
-    readonly todoId: number
-
-    @IsNotEmpty()
     @IsString()
     readonly text: string
 }
