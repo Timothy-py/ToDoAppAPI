a6fa92011447945ffd311a3ea7d7c9b68ec3ab94
Timothy-py <adeyeyetimothy33@gmail.com>
2023-02-08 23:28:07 +0100

    generated tag resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,9 @@ import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { TodoModule } from './todo/todo.module';
 import { CommentModule } from './comment/comment.module';
+import { TagModule } from './tag/tag.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule, ConfigModule.forRoot({isGlobal: true}), UserModule, TodoModule, CommentModule]
+  imports: [AuthModule, PrismaModule, ConfigModule.forRoot({isGlobal: true}), UserModule, TodoModule, CommentModule, TagModule]
 })
 export class AppModule {}
