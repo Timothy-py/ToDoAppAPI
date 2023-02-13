f2bbe380a24dd03a946a58d802bbe05104a34166
Timothy-py <adeyeyetimothy33@gmail.com>
2023-02-13 23:56:03 +0100

    implement tag controller

--- src/tag/tag.controller.ts
+++ src/tag/tag.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Request, UseGuards, Post, Body } from '@nestjs/common';
+import { JwtGuard } from 'src/auth/guard';
+import { CreateTagDto } from './dto';
+import { TagService } from './tag.service';
 
 @Controller('tag')
-export class TagController {}
+export class TagController {
+    constructor(private readonly tagService: TagService) {}
+
+    @UseGuards(JwtGuard)
+    @Post('')
+    createTag(@Body() tagDto: CreateTagDto, @Request() req){
+        console.log(tagDto)
+        console.log(req.user)
+        return "Tag created successfully"
+    }
+}
