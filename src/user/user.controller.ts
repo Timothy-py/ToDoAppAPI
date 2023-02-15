362195733ee8a55ed347816c4332eea65e3a38b0
Timothy-py <adeyeyetimothy33@gmail.com>
2023-02-15 23:54:54 +0100

    implement delete user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, ParseIntPipe, Request, UseGuards } from '@nestjs/common';
+import { Controller, Delete, Get, Param, ParseIntPipe, Request, UseGuards } from '@nestjs/common';
 import { JwtGuard } from 'src/auth/guard';
 import { UserService } from './user.service';
 
@@ -15,5 +15,11 @@ export class UserController {
     @Get(':id')
     getUser(@Param('id', ParseIntPipe) id: number, @Request() req){
         return this.userService.getUser(id, req.user.userId)
+    }
+
+    @UseGuards(JwtGuard)
+    @Delete(':id')
+    deleteMe(@Param('id', ParseIntPipe) id:number, @Request() req){
+        return "User Deleted successfully"
     }
 }
