975dadc6bcc870c387bb069b3e1ff2c51f0f3993
Timothy-py <adeyeyetimothy33@gmail.com>
2023-02-25 22:04:39 +0100

    fix dto

--- src/todo/dto/update.todo.dto.ts
+++ src/todo/dto/update.todo.dto.ts
@@ -1,10 +1,11 @@
+import { Status } from "./enum.status"
 
 export class UpdateTodoDto {
     readonly title?: string
 
     readonly description?: string
 
-    readonly status?: string
+    readonly status?: Status
 
     readonly tags?: []
 }
