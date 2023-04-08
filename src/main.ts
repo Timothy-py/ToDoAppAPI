de550ebb948713da14045fafa8ec069ea33779c4
Timothy-py <adeyeyetimothy33@gmail.com>
2023-04-09 00:33:25 +0100

    doc<api documentation>: implement swagger module #9

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,25 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
     // whitelist: true
   }))
+
+  // setup swagger
+  const config = new DocumentBuilder()
+      .setTitle('TodoVista API')
+      .setDescription('OpenAPI documentation for TodoVista API')
+      .setVersion('1.0')
+      .addTag('Todo')
+      .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
